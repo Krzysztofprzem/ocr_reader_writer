ba8021952db5bcbfa34de39a4c5a32ad567f53b9
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-11-24 00:52:52 +0100

    Created complete program. Added init dirs for input images and output .txt files. Added test catalog for tests

--- ocr_reader_writer.py
+++ ocr_reader_writer.py
@@ -11,9 +11,50 @@ def create_necessary_directories(path):
     try:
         os.makedirs(path)
     except OSError:
-        print ("The directory %s already exists" % path)
+        print("The directory %s already exists" % path)
     else:
-        print ("Successfully created the directory %s " % path)
+        print("Successfully created the directory %s " % path)
+
+
+def check_size(path):
+    file_size_B = os.stat(path).st_size
+    file_size_KB = file_size_B/1024.0
+    
+    if file_size_KB > 1024:
+        return False, file_size_B
+    else:
+        return True, file_size_B
+
+
+def compression(input_path, output_path, compression_factor=0.8):
+    im = Image.open(input_path)  
+
+    width, height = im.size  
+    width_new  = int(width*compression_factor)
+    height_new = int(height*compression_factor)
+    newsize = (width_new, height_new) 
+
+    im = im.resize(newsize) 
+    im.save(output_path)
+
+
+def compressator(path, compression_factor=0.8):
+    temp_path = "temp\\" + path.split("\\")[-1]
+    compression(path, temp_path)
+
+    while check_size(temp_path)[0] is False:
+        compression(temp_path, temp_path, compression_factor)
+
+    return temp_path
+
+
+def preprocess(filename_str):
+    good_size, _ = check_size(filename_str)
+    if good_size is False:
+        temp_path = compressator(filename_str)
+        return False, temp_path
+    else:
+        return True, filename_str
 
 
 def ocrspace_call(filename="kawaii.jpg", apikey="helloworld", language="jpn", printing=False):
@@ -25,15 +66,15 @@ def ocrspace_call(filename="kawaii.jpg", apikey="helloworld", language="jpn", pr
     :param printing: if True then printing debugging stuff
     :return: text from image
     """
-
     url_api = "https://api.ocr.space/parse/image"
+
     with open(filename, "rb") as f:
 
-        result = requests.post(url_api,
-                    files = {filename: f},
-                    data = {"apikey": apikey,
-                            "language": language})
+        files = {filename: f}
+        data = {"apikey": apikey, 
+                "language": language}
 
+        result = requests.post(url_api, files=files, data=data)
         result = result.content.decode()
         result = json.loads(result)
 
@@ -42,7 +83,8 @@ def ocrspace_call(filename="kawaii.jpg", apikey="helloworld", language="jpn", pr
 
         parsed_results = result.get("ParsedResults")[0]
         text_detected = parsed_results.get("ParsedText")
-        return text_detected
+
+    return text_detected
 
 
 def ocr_reader_writer():
@@ -60,17 +102,26 @@ def ocr_reader_writer():
     # Get paths to all images in input_path
     filenames = Path(input_path).rglob('*.jpg')
     for filename in filenames:
+        # Convert filename into string
+        filename_str = str(filename)
+
         # Create catalog tree in outputpath for each image 
-        index = str(filename).rfind("\\")
-        directory_path = output_path+str(filename)[:index]
+        index = filename_str.rfind("\\")
+        directory_path = output_path+filename_str[:index]
         create_necessary_directories(directory_path)
 
-        # Send image to ocr space and get text result
-        text_detected = ocrspace_call(str(filename), apikey, language, False)
-        print(text_detected)
+        # Check if image has size bigger than 1024KB
+        good_size, filename_new = preprocess(filename_str)
+
+        # Send image to ocr space
+        text_detected = ocrspace_call(filename_new, apikey, language, True)
+
+        # If image had size bigger than 1024KB remove temporary compressed image
+        if good_size is False:
+            os.remove(filename_new)
 
         # Save detected text into text file
-        outputtextfilepath = output_path+str(filename)[:-3]+".txt"
+        outputtextfilepath = output_path+filename_str[:-3]+"txt"
         textfile = open(outputtextfilepath, 'w', encoding='utf8')
         textfile.write(text_detected)
         textfile.close()
@@ -79,7 +130,7 @@ def ocr_reader_writer():
         time_start = time.time()
         while time.time() - time_start < interval:
             continue
-        break
+
 
 
 if __name__ == "__main__":
